d3d712b218fe4d0ace760a3dfafc4165f3f283e7
lenkton <lenktongm@gmail.com>
2025-07-06 01:14:31 +0300

    refactor: move finding the game into a middleware

--- handlers/games_api.go
+++ handlers/games_api.go
@@ -14,10 +14,13 @@ import (
 func BindGamesAPI(r *gin.Engine) {
 	r.GET("/games", indexGames)
 	r.POST("/games", createGame)
-	r.GET("/games/:id", getGame)
-	r.POST("/games/:id/join", joinGame)
-	r.POST("/games/:id/roll", rollDice)
-	r.GET("/games/:id/updates", connectToGameUpdates)
+	withGame := r.Group("/games/:id", middlewareFindGame)
+	{
+		withGame.GET("", getGame)
+		withGame.POST("/join", joinGame)
+		withGame.POST("/roll", rollDice)
+		withGame.GET("/updates", connectToGameUpdates)
+	}
 }
 
 func indexGames(c *gin.Context) {
@@ -30,12 +33,7 @@ type joinGameDTO struct {
 }
 
 func joinGame(c *gin.Context) {
-	id := c.Param("id")
-	game, err := models.FindGame(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Game Not Found"})
-		return
-	}
+	game := c.MustGet("game").(*models.Game)
 	var requestBody joinGameDTO
 	if err := c.ShouldBindJSON(&requestBody); err != nil {
 		// TODO: add sane error messages
@@ -56,13 +54,7 @@ func joinGame(c *gin.Context) {
 }
 
 func getGame(c *gin.Context) {
-	id := c.Param("id")
-	game, err := models.FindGame(id)
-	if err != nil {
-		// TODO: add some 404
-		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Game Not Found"})
-		return
-	}
+	game := c.MustGet("game").(*models.Game)
 	userStringID, err := c.Cookie("user_id")
 	var user *models.Player
 	if err == nil {
@@ -94,13 +86,18 @@ func createGame(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, game)
 }
 
-func rollDice(c *gin.Context) {
+func middlewareFindGame(c *gin.Context) {
 	id := c.Param("id")
 	game, err := models.FindGame(id)
 	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Game Not Found"})
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Game Not Found"})
 		return
 	}
+	c.Set("game", game)
+}
+
+func rollDice(c *gin.Context) {
+	game := c.MustGet("game").(*models.Game)
 	var requestBody struct {
 		Dice int `json:"dice"`
 	}
@@ -123,12 +120,7 @@ var upgrader = websocket.Upgrader{
 }
 
 func connectToGameUpdates(c *gin.Context) {
-	id := c.Param("id")
-	game, err := models.FindGame(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Game Not Found"})
-		return
-	}
+	game := c.MustGet("game").(*models.Game)
 	w, r := c.Writer, c.Request
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
