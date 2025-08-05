2c188413df8894382fc16ec0d3da7008cee9a5c7
Sergey Antonov <lenktongm@gmail.com>
2025-08-06 01:57:34 +0300

    style: rip MakeTurn into micro functions

--- pkg/services/turn/make_turn.go
+++ pkg/services/turn/make_turn.go
@@ -13,24 +13,35 @@ func MakeTurn(game *models.Game, player *models.Player, dice int) (*models.Turn,
 		return nil, errors.New("it is another player's turn")
 	}
 
-	turn := &models.Turn{
-		Dice:   dice,
-		Result: rand.Intn(dice) + 1,
-	}
+	turn := buildTurn(dice)
 
 	turn, err := models.MainStorage.CreateTurn(turn, game)
 	if err != nil {
 		return nil, fmt.Errorf("MakeTurn: error saving turn: %v", err)
 	}
 
+	updateNextPlayer(game)
+	publishNewTurn(turn, game)
+
+	return turn, err
+}
+
+func buildTurn(dice int) *models.Turn {
+	return &models.Turn{
+		Dice:   dice,
+		Result: rand.Intn(dice) + 1,
+	}
+}
+
+func updateNextPlayer(game *models.Game) {
 	// TODO: move this somewhere or call some fake save on this
 	nextPlayerIndex := len(game.Turns) % len(game.Players)
 	game.CurrentPlayer = game.Players[nextPlayerIndex]
+}
 
+func publishNewTurn(turn *models.Turn, game *models.Game) {
 	game.News.Publish(models.NewsMessage{
 		"type": "newTurn",
 		"turn": turn,
 	})
-
-	return turn, err
 }
